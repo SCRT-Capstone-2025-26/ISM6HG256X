ff35c29835c968d03f331536c671ff7b53d1ce0f
5t0n3 <31430937+5t0n3@users.noreply.github.com>
2026-02-26 13:26:44 -0800

    feat: method to disable sflp

--- src/ISM6HG256XSensor.h
+++ src/ISM6HG256XSensor.h
@@ -186,6 +186,7 @@ class ISM6HG256XSensor {
     ISM6HG256XStatusTypeDef Get_HG_X_AxesRaw(ISM6HG256X_AxesRaw_t *Value);
     ISM6HG256XStatusTypeDef HG_X_GetAxes(ISM6HG256X_Axes_t *Acceleration);
     ISM6HG256XStatusTypeDef Enable_SFLP();
+    ISM6HG256XStatusTypeDef Disable_SFLP();
     ISM6HG256XStatusTypeDef FIFO_Get_Num_Samples(uint16_t *NumSamples);
     ISM6HG256XStatusTypeDef FIFO_Get_Full_Status(uint8_t *Status);
     ISM6HG256XStatusTypeDef FIFO_Set_INT1_FIFO_Full(uint8_t Status);
